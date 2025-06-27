31c6e66585c2ea1bdf72a57ff428bda394384bd3
keresztesizoli <keresztesi@gmail.com>
2025-06-27 13:51:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'form.html'));
+});
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
